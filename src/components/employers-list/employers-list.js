a9d1e6b09fdf2b3af7493e21b2e2923a9fcdbd77
alpetrushkin <104225655+alpetrushkin@users.noreply.github.com>
2022-12-27 20:16:47 +0300

    верстка

---
+++ src/components/employers-list/employers-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './employers-list.css'
+
+import EmployersListItem from "../employers-list-item/employers-list-item";
+
+const EmployersList = () => {
+   return (
+      <ul className="app-list list-group">
+         <EmployersListItem/>
+         <EmployersListItem/>
+         <EmployersListItem/>
+      </ul>
+   );
+};
+
+export default EmployersList;
